b7fc323c0c5b83ed7cade84cfc54e6b168b8f389
rvanxer <rajatverma1995@gmail.com>
2023-10-22 09:29:59 -0400

    Changes before standard params.yaml

--- code/export.R
+++ code/export.R
@@ -1,2 +1,7 @@
-library(dplyr)
+library(arrow)
+library(data.table)
 
+print('Exporting data to RData format')
+df <- read_parquet('../data/dashboard/ses.parquet')
+# df <- as.data.table(df)
+# save(df, file = '../data/dashboard/ses.RData')
